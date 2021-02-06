e23b5ed853cfa7315ddfdaf15803f0321031a0bd
mswgen <54498184+mswgen@users.noreply.github.com>
2021-02-06 18:02:20 +0900

    토큰 길이 변경

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.verifyQueue = new Discord.Collection();
 function tokenGen () {
     let chars = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     let t = [];
-    for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < 100; i++) {
         t.push(chars[Math.floor(Math.random() * chars.length)]);
     }
     if (client.verifyQueue.get(t)) t = tokenGen();
