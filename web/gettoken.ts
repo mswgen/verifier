ad14dce824e22f0a41ba861636cd701eeeebf42f
mswgen <54498184+mswgen@users.noreply.github.com>
2021-06-22 08:47:49 +0900

    fix: redirect uri bug

--- web/gettoken.ts
+++ web/gettoken.ts
@@ -18,7 +18,7 @@ export default {
           client_secret: process.env.CLIENT_SECRET,
           code: post,
           scope: 'identify guilds',
-          redirect_uri: 'https://verifier.teamint.xyz/static/html/new.html',
+          redirect_uri: 'https://verifier.teamint.xyz',
           grant_type: 'authorization_code'
         }), {
           headers: {
