5bf66ef64a9b5c33dc71d9d4ab81177ab9068174
mswgen <54498184+mswgen@users.noreply.github.com>
2021-07-05 10:56:16 +0900

    fix: update links

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -17,7 +17,7 @@ function init () {
   if (getParam('state')) {
     if (getParam('state') == 'dash') {
       if (!getParam('code') && !localStorage.getItem('discord')) {
-        document.location.href = 'https://discord.com/api/oauth2/authorize?client_id=791863119843819520&redirect_uri=https%3A%2F%2Fverifier.teamint.xyz&response_type=code&scope=identify%20guilds'
+        document.location.href = 'https://discord.com/api/oauth2/authorize?client_id=791863119843819520&redirect_uri=https%3A%2F%2Fverifier.teamint.xyz&response_type=code&scope=identify%20guilds&state=dash'
         return
       }
       fetchPage('/static/html/mounts/guildselect.html').then(() => {
