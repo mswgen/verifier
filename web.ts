099b60e619df82cd72d3c7d877aa0cc5c2a373e4
mswgen <54498184+mswgen@users.noreply.github.com>
2021-07-05 09:27:16 +0900

    chore: upgrade deps

--- web.ts
+++ web.ts
@@ -1,11 +1,9 @@
 import https from 'https'
 import http from 'http'
-import axios from 'axios'
 import url from 'url'
 import fs from 'fs/promises'
 import path from 'path'
 import Discord from 'discord.js'
-import io from 'socket.io'
 import type mongodb from 'mongodb'
 export default {
   start: async (client: Discord.Client, db: { serverConf: mongodb.Collection, notifications: mongodb.Collection }) => {
@@ -194,117 +192,6 @@ export default {
     })
     httpsServer.listen(4430, () => {
       console.log('https server started')
-    })
-    const socketio = require('socket.io')
-    const io = socketio(httpsServer)
-    io.on('connection', (socket: io.Socket) => {
-      socket.on('init', token => {
-        axios.get('https://discord.com/api/users/@me', {
-          headers: {
-            Authorization: token
-          }
-        }).then(() => {
-          (socket as any).token = token
-          socket.emit('validated')
-        }).catch(() => {
-          socket.disconnect()
-        })
-      })
-      socket.on('servers', () => {
-        let resp: any = []
-        axios.get('https://discord.com/api/users/@me/guilds', {
-          headers: {
-            Authorization: (socket as any).token
-          }
-        }).then(res => {
-          for (let x of res.data) {
-            if ((new Discord.Permissions(x.permissions)).has(['MANAGE_GUILD']) && client.guilds.cache.get(x.id) && client.guilds.cache.get(x.id)!.me!.hasPermission(['MANAGE_ROLES', 'VIEW_CHANNEL', 'READ_MESSAGE_HISTORY', 'MANAGE_MESSAGES'])) {
-              resp.push({
-                id: x.id,
-                name: x.name.replace(/</gi, '&lt;').replace(/>/gi, '&gt;'),
-                icon: x.icon ? `https://cdn.discordapp.com/icons/${x.id}/${x.icon}.webp` : 'https://cdn.discordapp.com/embed/avatars/0.png'
-              })
-            }
-          }
-          socket.emit('servers-resp', resp)
-        })
-      })
-      socket.on('getInfo', guildId => {
-        axios.get('https://discord.com/api/users/@me/guilds', {
-          headers: {
-            Authorization: (socket as any).token
-          }
-        }).then(async x => {
-          if (new Discord.Permissions(x.data.find((a: Discord.Guild) => a.id == guildId).permissions).has('MANAGE_GUILD')) {
-            socket.emit('info', await db.serverConf.findOne({ _id: guildId }))
-          }
-        })
-      })
-      socket.on('subm', data => {
-        axios.get('https://discord.com/api/users/@me/guilds', {
-          headers: {
-            Authorization: (socket as any).token
-          }
-        }).then(res => {
-          if (new Discord.Permissions(res.data.find((a: any) => a.id == data.guildId).permissions).has('MANAGE_GUILD')) {
-            if (!client.guilds.cache.get(data.guildId)!.channels.cache.find(x => x.id == data.channelId)) {
-              socket.emit('submitted', '입력한 채널을 찾을 수 없어요')
-              return
-            }
-            if (client.guilds.cache.get(data.guildId)!.channels.cache.find(x => x.id == data.channelId)!.type != 'text' && client.guilds.cache.get(data.guildId)!.channels.cache.find(x => x.id == data.channelId)!.type != 'news') {
-              socket.emit('submitted', '채팅 채널, 공지 채널만 입력할 수 있어요')
-              return
-            }
-            if (!client.guilds.cache.get(data.guildId)!.channels.cache.find(x => x.id == data.channelId)!.permissionsFor(client.user!.id)!.has(['SEND_MESSAGES', 'ADD_REACTIONS', 'MANAGE_MESSAGES'])) {
-              socket.emit('submitted', '봇이 입력한 채널에 대해서 메세지 보내기, 메세지 관리, 반응 추가하기 권한이 없어요')
-              return
-            }
-            (client.guilds.cache.get(data.guildId)!.channels.cache.find(x => x.id == data.channelId) as Discord.TextChannel).messages.fetch(data.messageId).catch(() => {
-              socket.emit('submitted', '입력한 메세지를 찾을 수 없어요')
-            }).then(async msg => {
-              if (data.unverifiedRole && !client.guilds.cache.get(data.guildId)!.roles.cache.get(data.unverifiedRole)) {
-                socket.emit('submitted', '입력한 미인증 역할을 찾을 수 없어요')
-                return
-              }
-              if (data.unverifiedRole && client.guilds.cache.get(data.guildId)!.roles.cache.get(data.unverifiedRole)!.position >= client.guilds.cache.get(data.guildId)!.me!.roles.highest.position) {
-                socket.emit('submitted', '봇이 입력한 미인증 역할을 지급할 수 없어요. 역할 순서를 변경해주세요.')
-                return
-              }
-              if (data.verifiedRole && !client.guilds.cache.get(data.guildId)!.roles.cache.get(data.verifiedRole)) {
-                socket.emit('submitted', '입력한 인증 완료 역할을 찾을 수 없어요')
-                return
-              }
-              if (data.verifiedRole && client.guilds.cache.get(data.guildId)!.roles.cache.get(data.verifiedRole)!.position >= client.guilds.cache.get(data.guildId)!.me!.roles.highest.position) {
-                socket.emit('submitted', '봇이 입력한 인증 완료 역할을 지급할 수 없어요. 역할 순서를 변경해주세요.')
-                return
-              }
-              if (!data.msg) {
-                socket.emit('submitted', '인증 페이지 내용을 입력해주세요.')
-              }
-              if (data.msg.length > 200) {
-                socket.emit('submitted', '인증 페이지 내용은 최대 200자까지 입력할 수 있어요.')
-              }
-              if (data.verifiedMsg && data.verifiedMsg.length > 2000) {
-                socket.emit('submitted', '인증 완료 메세지는 최대 2000자까지 입력할 수 있어요.')
-              }
-              await db.serverConf.updateOne({ _id: data.guildId }, {
-                $set: {
-                  channelId: data.channelId,
-                  messageId: data.messageId,
-                  unverifiedRole: data.unverifiedRole,
-                  verifiedRole: data.verifiedRole,
-                  verifiedMsg: data.verifiedMsg,
-                  msg: data.msg
-                }
-              });
-              await (msg as Discord.Message).react('✅')
-              socket.emit('submitted')
-            })
-          } else {
-            socket.emit('submitted', '해당 서버의 설정은 바꿀 수 없어요')
-          }
-        })
-      })
     })
   }
 }
