d694ea0ec83b2683962bc16257af7be9f9ce31c7
mswgen <54498184+mswgen@users.noreply.github.com>
2021-07-05 12:33:50 +0900

    feat(web): server push

--- web/root.ts
+++ web/root.ts
@@ -6,6 +6,13 @@ export default {
   pathname: '/',
   method: 'GET',
   run: async (client: Discord.Client, _db: any, req: http.IncomingMessage, res: http.ServerResponse) => {
+    res.setHeader('Link', [
+      '</static/css/compiled.css>; rel=preload; as=style',
+      '</static/js/spa.js>; rel=preload; as=script',
+      '</static/image/svg/light.svg>; rel=preload; as=image',
+      '</static/image/svg/mountain.svg>; rel=preload; as=image',
+      '</static/image/webp/inticon.webp>; rel=preload; as=image'
+    ])
     res.writeHead(200, {
       // 'strict-transport-security': 'max-age=86400; includeSubDomains; preload',
       'content-type': 'text/html; charset=UTF-8'
